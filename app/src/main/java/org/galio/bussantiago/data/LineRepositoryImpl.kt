f4c356d60b1b0addef11773d3641ed4bb16c6118
José Ángel Maneiro <joseangelmaneiro@yahoo.es>
2019-06-25 20:48:46 +0200

    Obtain lines from ApiClient (#5)

--- app/src/main/java/org/galio/bussantiago/data/LineRepositoryImpl.kt
+++ app/src/main/java/org/galio/bussantiago/data/LineRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package org.galio.bussantiago.data
 
+import org.galio.bussantiago.data.api.ApiClient
 import org.galio.bussantiago.domain.LineRepository
 
-class LineRepositoryImpl : LineRepository {
+internal class LineRepositoryImpl(
+  private val apiClient: ApiClient
+) : LineRepository {
 
   override fun getLines(): List<String> {
     return listOf("Line1", "Line2")
