e72f5a157b6ccf8b0a32ad226e24942d12ae62cd
José Ángel Maneiro <joseangelmaneiro@yahoo.es>
2019-06-25 20:48:46 +0200

    Obtain lines from ApiClient (#5)

--- app/src/main/java/org/galio/bussantiago/di/AppModule.kt
+++ app/src/main/java/org/galio/bussantiago/di/AppModule.kt
@@ -1,9 +1,11 @@
 package org.galio.bussantiago.di
 
 import org.galio.bussantiago.data.LineRepositoryImpl
+import org.galio.bussantiago.data.api.ApiClient
 import org.galio.bussantiago.domain.LineRepository
 import org.koin.dsl.module
 
 val appModule = module {
-  single<LineRepository> { LineRepositoryImpl() }
+  factory { ApiClient() }
+  single<LineRepository> { LineRepositoryImpl(get()) }
 }
