ca5c5d46758978c61a3e935597fbfa5f63c7d229
Jose Angel Maneiro <joseangelmaneiro@yahoo.es>
2019-07-15 16:09:38 +0200

    Implemented GetLineDetails interactor

--- app/src/main/java/org/galio/bussantiago/menu/MenuFragment.kt
+++ app/src/main/java/org/galio/bussantiago/menu/MenuFragment.kt
@@ -1,5 +1,6 @@
 package org.galio.bussantiago.menu
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
@@ -7,8 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.menu_fragment.*
 import org.galio.bussantiago.R
+import org.galio.bussantiago.common.Status
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MenuFragment : DialogFragment() {
+
+  private val viewModel: MenuViewModel by viewModel()
 
   companion object {
     private const val ID_KEY = "id_key"
@@ -30,7 +35,23 @@ class MenuFragment : DialogFragment() {
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
 
+    viewModel.lineDetails.observe(this, Observer {
+      it?.let { resourceLineDetails ->
+        when (resourceLineDetails.status) {
+          Status.LOADING -> {
+            // Todo Not Implemented
+          }
+          Status.SUCCESS -> {
+            textMenu.text = it.toString()
+          }
+          Status.ERROR -> {
+            // Todo Not Implemented
+          }
+        }
+      }
+    })
+
     val id = arguments?.get(ID_KEY) as Int
-    textMenu.text = "Menu: $id"
+    viewModel.loadLineDetails(id)
   }
 }
