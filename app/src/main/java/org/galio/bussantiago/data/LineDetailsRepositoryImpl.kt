036d8c78866e98449bd1b80be453b80f029de0f1
Jose Angel Maneiro <joseangelmaneiro@yahoo.es>
2019-07-26 12:04:41 +0200

    Validate single source fo truth with PrefetchLocalData

--- app/src/main/java/org/galio/bussantiago/data/LineDetailsRepositoryImpl.kt
+++ app/src/main/java/org/galio/bussantiago/data/LineDetailsRepositoryImpl.kt
@@ -2,21 +2,27 @@ package org.galio.bussantiago.data
 
 import org.galio.bussantiago.common.Either
 import org.galio.bussantiago.data.api.ApiClient
+import org.galio.bussantiago.data.cache.LineDetailsCache
+import org.galio.bussantiago.data.entity.LineDetailsEntity
 import org.galio.bussantiago.data.mapper.LineDetailsMapper
 import org.galio.bussantiago.domain.model.LineDetails
 import org.galio.bussantiago.domain.repository.LineDetailsRepository
 
 internal class LineDetailsRepositoryImpl(
   private val apiClient: ApiClient,
-  private val mapper: LineDetailsMapper
+  private val mapper: LineDetailsMapper,
+  private val cache: LineDetailsCache
 ) : LineDetailsRepository {
 
   override fun getLineDetails(id: Int): Either<Exception, LineDetails> {
-    val response = apiClient.getLineDetails(id)
-    return if (response.isRight) {
-      Either.Right(mapper.toDomain(response.rightValue))
-    } else {
-      Either.Left(response.leftValue)
-    }
+    return object : PrefetchLocalData<LineDetailsEntity, LineDetails>() {
+      override fun loadFromLocal() = cache.get(id)
+
+      override fun loadFromService() = apiClient.getLineDetails(id)
+
+      override fun saveServiceResult(item: LineDetailsEntity) {
+        cache.save(id, mapper.toDomain(item))
+      }
+    }.load()
   }
 }
