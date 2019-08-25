e73c941cd57bece3ef47917a688e1d01abf84a58
Jose Angel Maneiro <joseangelmaneiro@yahoo.es>
2019-08-25 20:09:29 +0200

    Implemented favorites module

---
+++ app/src/main/java/org/galio/bussantiago/features/favorites/BusStopFavoritesAdapter.kt
@@ -0,0 +1,42 @@
+package org.galio.bussantiago.features.favorites
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import org.galio.bussantiago.R
+import org.galio.bussantiago.common.inflate
+import org.galio.bussantiago.domain.model.BusStopFavorite
+
+class BusStopFavoritesAdapter(
+  private val items: List<BusStopFavorite>,
+  private val listener: (BusStopFavorite) -> Unit
+) : RecyclerView.Adapter<BusStopFavoritesAdapter.ItemViewHolder>() {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    val itemViewHolder = parent.inflate(R.layout.favorite_item)
+    return ItemViewHolder(itemViewHolder)
+  }
+
+  override fun getItemCount() = items.size
+
+  override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+    holder.bind(items[position])
+  }
+
+  inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val codeView = itemView.findViewById(R.id.codeTextView) as TextView
+    private val nameView = itemView.findViewById(R.id.nameTextView) as TextView
+
+    init {
+      itemView.setOnClickListener {
+        listener(items[adapterPosition])
+      }
+    }
+
+    fun bind(busStopFavorite: BusStopFavorite) {
+      codeView.text = busStopFavorite.code
+      nameView.text = busStopFavorite.name
+    }
+  }
+}
---
+++ app/src/test/java/org/galio/bussantiago/features/favorites/FavoritesViewModelTest.kt
@@ -0,0 +1,57 @@
+package org.galio.bussantiago.features.favorites
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import org.galio.bussantiago.common.Either
+import org.galio.bussantiago.common.Resource
+import org.galio.bussantiago.common.executor.SyncInteractorExecutor
+import org.galio.bussantiago.domain.model.BusStopFavorite
+import org.galio.bussantiago.util.mock
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+
+class FavoritesViewModelTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private val executor = SyncInteractorExecutor()
+    private val getBusStopFavorites = mock<GetBusStopFavorites>()
+    private val observer = mock<Observer<Resource<List<BusStopFavorite>>>>()
+
+    private lateinit var viewModel: FavoritesViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = FavoritesViewModel(executor, getBusStopFavorites)
+        viewModel.busStopFavorites.observeForever(observer)
+    }
+
+    @Test
+    fun `if all goes well, the favorites are loaded correctly`() {
+        val request = Unit
+        val busStopFavoritesStub = mock<List<BusStopFavorite>>()
+        `when`(getBusStopFavorites(request)).thenReturn(Either.Right(busStopFavoritesStub))
+
+        viewModel.loadFavorites()
+
+        verify(observer).onChanged(Resource.loading())
+        verify(observer).onChanged(Resource.success(busStopFavoritesStub))
+    }
+
+    @Test
+    fun `fire the exception received`() {
+        val request = Unit
+        val exception = Exception("Fake exception")
+        `when`(getBusStopFavorites(request)).thenReturn(Either.Left(exception))
+
+        viewModel.loadFavorites()
+
+        verify(observer).onChanged(Resource.loading())
+        verify(observer).onChanged(Resource.error(exception))
+    }
+}
