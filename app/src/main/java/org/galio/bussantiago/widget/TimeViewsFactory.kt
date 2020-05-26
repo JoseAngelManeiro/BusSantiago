c6353f9c588fb501470a93d951e9c818c08e61b9
José Ángel Maneiro <joseangelmaneiro@yahoo.es>
2020-05-26 16:54:25 +0200

    Widget (#45)
    * Added HttpLoggintInterceptor to the ApiClient
    
    * Implemented WidgetActivity
    
    * Implemented WidgetPrefsHelper
    
    * Implemented WidgetProvider

---
+++ app/src/main/java/org/galio/bussantiago/widget/TimeViewsFactory.kt
@@ -0,0 +1,133 @@
+package org.galio.bussantiago.widget
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.DisplayMetrics
+import android.view.View
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import org.galio.bussantiago.R
+import org.galio.bussantiago.common.model.SynopticModel
+import org.galio.bussantiago.features.times.LineRemainingTimeModel
+import org.galio.bussantiago.features.times.getDescriptionByMinutes
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class TimeViewsFactory(
+  private val context: Context,
+  intent: Intent
+) : RemoteViewsService.RemoteViewsFactory {
+
+  private val widgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+    AppWidgetManager.INVALID_APPWIDGET_ID)
+  private val widgetPrefsHelper = WidgetPrefsHelper(context)
+  private val stopCode = widgetPrefsHelper.getCode(widgetId)
+
+  private lateinit var lineRemainingTimeModels: MutableList<LineRemainingTimeModel>
+
+  override fun onCreate() {
+    lineRemainingTimeModels = mutableListOf()
+  }
+
+  override fun onDataSetChanged() {
+    val appWidgetManager = AppWidgetManager.getInstance(context)
+
+    val remoteViews = RemoteViews(context.packageName, R.layout.app_widget)
+
+    // Init loading mode
+    remoteViews.setViewVisibility(R.id.refresh_button, View.GONE)
+    remoteViews.setViewVisibility(R.id.progressBar, View.VISIBLE)
+    appWidgetManager.updateAppWidget(widgetId, remoteViews)
+
+    // Obtain times from service
+    val tempList = mutableListOf<LineRemainingTimeModel>()
+    val result = ObtainJson()
+      .call("http://app.tussa.org/tussa/api/lineas/0/parada/$stopCode")
+    if (result != null) {
+      tempList.addAll(parseToItemWidgets(result))
+    }
+
+    // Finish loading mode
+    remoteViews.setViewVisibility(R.id.progressBar, View.GONE)
+    remoteViews.setViewVisibility(R.id.refresh_button, View.VISIBLE)
+    val formatter = SimpleDateFormat("HH:mm", Locale.US)
+    val hour = formatter.format(Calendar.getInstance().timeInMillis)
+    remoteViews.setTextViewText(R.id.hourSync_textview, hour)
+    appWidgetManager.updateAppWidget(widgetId, remoteViews)
+
+    // Save sync hour in preferences
+    widgetPrefsHelper.save(hour = hour, widgetId = widgetId)
+
+    // At the end of the data load, we must rebuild our widget,
+    // so that when it is restored it does not suffer loss of instances.
+    WidgetProvider.updateWidget(context, appWidgetManager, widgetId)
+
+    // We refresh values from our list, which will be collected from the getViewAt()
+    // method to paint the result in the widget.
+    lineRemainingTimeModels = tempList
+  }
+
+  override fun getViewAt(position: Int): RemoteViews {
+    return RemoteViews(context.packageName, R.layout.time_item_widget).apply {
+      val lineRemainingTimeModel = lineRemainingTimeModels[position]
+
+      val circleSizePx = dpToPixel(40) // synoptic_size = 40dp
+      val bitmap = Bitmap.createBitmap(circleSizePx, circleSizePx, Bitmap.Config.ARGB_8888)
+      setImageViewBitmap(R.id.lineWidgetImageView, bitmap)
+      val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+      val canvas = Canvas(bitmap)
+      paint.color = Color.parseColor(lineRemainingTimeModel.synopticModel.style)
+      canvas.drawCircle(circleSizePx.toFloat() / 2, circleSizePx.toFloat() / 2,
+        (circleSizePx.toFloat() / 2) - 1, paint)
+
+      setTextViewText(R.id.lineWidgetTextView,
+        lineRemainingTimeModel.synopticModel.synoptic.removePrefix("L"))
+
+      setTextViewText(R.id.timeWidgetTextView,
+        getDescriptionByMinutes(lineRemainingTimeModel.minutesUntilNextArrival))
+    }
+  }
+
+  private fun dpToPixel(dp: Int): Int {
+    return dp * (context.resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
+  }
+
+  private fun parseToItemWidgets(jsonString: String): List<LineRemainingTimeModel> {
+    val arrayListTimes = mutableListOf<LineRemainingTimeModel>()
+    val jsonObjectResult = JSONObject(jsonString)
+    val jsonArrayLines = jsonObjectResult.getJSONArray("lineas")
+    var jsonObjectLine: JSONObject
+    for (i in 0 until jsonArrayLines.length()) {
+      jsonObjectLine = jsonArrayLines[i] as JSONObject
+      arrayListTimes.add(
+        LineRemainingTimeModel(
+          SynopticModel(
+            jsonObjectLine.getString("sinoptico"),
+            jsonObjectLine.getString("estilo")
+          ),
+          jsonObjectLine.getInt("minutosProximoPaso")
+        )
+      )
+    }
+    return arrayListTimes
+  }
+
+  override fun getCount() = lineRemainingTimeModels.size
+
+  override fun getViewTypeCount() = 1
+
+  override fun onDestroy() { lineRemainingTimeModels.clear() }
+
+  override fun getLoadingView(): RemoteViews? = null
+
+  override fun getItemId(position: Int) = position.toLong()
+
+  override fun hasStableIds() = true
+}
---
+++ app/src/main/java/org/galio/bussantiago/widget/WidgetActivity.kt
@@ -0,0 +1,69 @@
+package org.galio.bussantiago.widget
+
+import android.appwidget.AppWidgetManager
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.widget_activity.*
+import org.galio.bussantiago.R
+import org.galio.bussantiago.data.local.FavoriteDataSource
+import org.galio.bussantiago.domain.model.BusStopFavorite
+import org.galio.bussantiago.features.favorites.BusStopFavoritesAdapter
+import org.koin.android.ext.android.inject
+
+class WidgetActivity : AppCompatActivity() {
+
+  private val favoriteDataSource: FavoriteDataSource by inject()
+
+  private var widgetId: Int = 0
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.widget_activity)
+
+    // First we set a default value
+    setResult(RESULT_CANCELED)
+
+    val params = intent.extras
+    if (params != null) {
+      // Get the ID of the widget that is being configured
+      widgetId = params.getInt(
+        AppWidgetManager.EXTRA_APPWIDGET_ID,
+        AppWidgetManager.INVALID_APPWIDGET_ID
+      )
+      if (widgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+        val busStopFavorites = favoriteDataSource.getAll()
+        if (busStopFavorites.isEmpty()) {
+          noFavoritesTextView.visibility = View.VISIBLE
+        } else {
+          favoritesRecyclerView.adapter =
+            BusStopFavoritesAdapter(busStopFavorites) { onBusStopFavoriteClick(it) }
+        }
+      } else {
+        throw IllegalArgumentException(this::class.java.simpleName + ": Invalid App Widget ID")
+      }
+    } else {
+      throw NullPointerException(this::class.java.simpleName + ": Null Intent params")
+    }
+  }
+
+  private fun onBusStopFavoriteClick(busStopFavorite: BusStopFavorite) {
+    val widgetPrefsHelper = WidgetPrefsHelper(this)
+    // Save stop code and name in preferences linked to widget id
+    widgetPrefsHelper.save(
+      code = busStopFavorite.code,
+      name = busStopFavorite.name,
+      widgetId = widgetId
+    )
+
+    // Invoke updateWidget to repaint the widget view
+    val appWidgetManager = AppWidgetManager.getInstance(this)
+    WidgetProvider.updateWidget(this, appWidgetManager, widgetId)
+
+    val intent = Intent()
+    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+    setResult(RESULT_OK, intent)
+    finish()
+  }
+}
