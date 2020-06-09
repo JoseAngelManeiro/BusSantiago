d3d8eb5558a8dd9fd8e1559140ec6ba1f7b76d76
Jose Angel Maneiro <joseangelmaneiro@yahoo.es>
2020-06-09 17:56:51 +0200

    Implemented AboutFragment

---
+++ app/src/main/java/org/galio/bussantiago/features/about/AboutFragment.kt
@@ -0,0 +1,45 @@
+package org.galio.bussantiago.features.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.about_fragment.*
+import org.galio.bussantiago.R
+
+class AboutFragment : DialogFragment() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
+  }
+
+  override fun onStart() {
+    super.onStart()
+    dialog?.let { dialog ->
+      val width = ViewGroup.LayoutParams.MATCH_PARENT
+      val height = ViewGroup.LayoutParams.MATCH_PARENT
+      dialog.window?.setLayout(width, height)
+    }
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.about_fragment, container, false)
+  }
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    setUpToolbar()
+  }
+
+  private fun setUpToolbar() {
+    toolbar.title = getString(R.string.about)
+    toolbar.setNavigationIcon(R.drawable.ic_close)
+    toolbar.setNavigationOnClickListener { dismiss() }
+  }
+}
