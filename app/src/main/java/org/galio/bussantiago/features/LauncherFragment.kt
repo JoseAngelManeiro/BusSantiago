449a712ba4e7dbad99111ea291700cb9e4daf9d5
José Ángel Maneiro <joseangelmaneiro@yahoo.es>
2020-11-27 19:29:42 +0100

    Create settings screen (#66)
    * Setup integration tests environment (#63)
    
    * Setup integration test environment
    
    * Added integration tests
    
    * Create settings screen (#64)
    
    * Added settings option to home menu
    
    * Implement Preferences to save settings option selected
    
    * Apply Preferences to launcher logic
    
    * Review lint warnings
    
    * Upgrade version: 4.0.2

--- app/src/main/java/org/galio/bussantiago/features/LauncherFragment.kt
+++ app/src/main/java/org/galio/bussantiago/features/LauncherFragment.kt
@@ -8,11 +8,13 @@ import androidx.fragment.app.Fragment
 import org.galio.bussantiago.R
 import org.galio.bussantiago.common.navigateSafe
 import org.galio.bussantiago.data.local.FavoriteDataSource
+import org.galio.bussantiago.framework.SettingsPreferences
 import org.koin.android.ext.android.inject
 
 class LauncherFragment : Fragment() {
 
   private val favoriteDataSource: FavoriteDataSource by inject()
+  private val settingsPreferences: SettingsPreferences by inject()
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -26,10 +28,13 @@ class LauncherFragment : Fragment() {
     super.onActivityCreated(savedInstanceState)
 
     val busStopFavorites = favoriteDataSource.getAll()
-    if (busStopFavorites.isEmpty()) {
-      navigateSafe(R.id.actionShowLines)
-    } else {
+    val homeScreenSelected = settingsPreferences.getHomeScreenType()
+    if (homeScreenSelected == SettingsPreferences.FAVORITES_HOME_VALUE &&
+      busStopFavorites.isNotEmpty()
+    ) {
       navigateSafe(R.id.actionShowFavorites)
+    } else {
+      navigateSafe(R.id.actionShowLines)
     }
   }
 }
