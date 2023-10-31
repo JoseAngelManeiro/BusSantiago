956b72d24831a85fc21d17846998cc1e4483531a
José Ángel Maneiro <joseangel.maneiro@xing.com>
2023-10-31 12:23:52 +0100

    Refactor and test LineRepository

--- core/src/main/java/org/galio/bussantiago/data/LineRepositoryImpl.kt
+++ core/src/main/java/org/galio/bussantiago/data/LineRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package org.galio.bussantiago.data
 
 import org.galio.bussantiago.Either
+import org.galio.bussantiago.Either.Left
+import org.galio.bussantiago.Either.Right
 import org.galio.bussantiago.data.api.ApiClient
 import org.galio.bussantiago.data.cache.LineCache
-import org.galio.bussantiago.data.entity.LineEntity
 import org.galio.bussantiago.data.mapper.LineMapper
 import org.galio.bussantiago.domain.model.Line
 import org.galio.bussantiago.domain.repository.LineRepository
@@ -15,16 +16,18 @@ class LineRepositoryImpl(
 ) : LineRepository {
 
   override fun getLines(): Either<Exception, List<Line>> {
-    return object : PrefetchLocalData<List<LineEntity>, List<Line>>() {
-      override fun loadFromLocal() = cache.getAll()
-
-      override fun shouldFetch(data: List<Line>?) = data.isNullOrEmpty()
-
-      override fun loadFromService() = apiClient.getLines()
-
-      override fun saveServiceResult(item: List<LineEntity>) {
-        cache.save(item.map { mapper.toDomain(it) })
+    val localData = cache.getAll()
+    return if (localData.isNotEmpty()) {
+      Right(localData)
+    } else {
+      val serviceResult = apiClient.getLines()
+      if (serviceResult.isRight) {
+        val lines = serviceResult.rightValue.map { mapper.toDomain(it) }
+        cache.save(lines)
+        Right(lines)
+      } else {
+        Left(serviceResult.leftValue)
       }
-    }.load()
+    }
   }
 }
