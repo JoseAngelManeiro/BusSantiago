10c57ff3860c3c616ded8f0a6e157217beee3156
José Ángel Maneiro <joseangel.maneiro@xing.com>
2023-10-31 12:09:45 +0100

    Refactor and test LineDetailsRepository

--- core/src/main/java/org/galio/bussantiago/data/LineDetailsRepositoryImpl.kt
+++ core/src/main/java/org/galio/bussantiago/data/LineDetailsRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package org.galio.bussantiago.data
 
 import org.galio.bussantiago.Either
+import org.galio.bussantiago.Either.Left
+import org.galio.bussantiago.Either.Right
 import org.galio.bussantiago.data.api.ApiClient
 import org.galio.bussantiago.data.cache.LineDetailsCache
-import org.galio.bussantiago.data.entity.LineDetailsEntity
 import org.galio.bussantiago.data.mapper.LineDetailsMapper
 import org.galio.bussantiago.domain.model.LineDetails
 import org.galio.bussantiago.domain.repository.LineDetailsRepository
@@ -15,16 +16,18 @@ class LineDetailsRepositoryImpl(
 ) : LineDetailsRepository {
 
   override fun getLineDetails(id: Int): Either<Exception, LineDetails> {
-    return object : PrefetchLocalData<LineDetailsEntity, LineDetails>() {
-      override fun loadFromLocal() = cache.get(id)
-
-      override fun shouldFetch(data: LineDetails?) = data == null
-
-      override fun loadFromService() = apiClient.getLineDetails(id)
-
-      override fun saveServiceResult(item: LineDetailsEntity) {
-        cache.save(id, mapper.toDomain(item))
+    val localData = cache.get(id)
+    return if (localData != null) {
+      Right(localData)
+    } else {
+      val serviceResult = apiClient.getLineDetails(id)
+      if (serviceResult.isRight) {
+        val lineDetails = mapper.toDomain(serviceResult.rightValue)
+        cache.save(id, lineDetails)
+        Right(lineDetails)
+      } else {
+        Left(serviceResult.leftValue)
       }
-    }.load()
+    }
   }
 }
