c16ddc78760b837934f4ed7965415de0d4a98e89
José Ángel Maneiro <joseangel.maneiro@xing.com>
2025-04-14 19:03:51 +0200

    Increase unit tests widget module coverage

--- shared/src/test/java/org/galio/bussantiago/shared/TimesFactoryTest.kt
+++ shared/src/test/java/org/galio/bussantiago/shared/TimesFactoryTest.kt
@@ -1,5 +1,63 @@
 package org.galio.bussantiago.shared
 
+import org.galio.bussantiago.core.model.BusStopRemainingTimes
+import org.galio.bussantiago.core.model.Coordinates
+import org.galio.bussantiago.core.model.LineRemainingTime
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class TimesFactoryTest {
 
+  private val timesFactory = TimesFactory()
+
+  @Test
+  fun `should create the list of models as expected`() {
+    val lineRemainingTime1 = lineRemainingTime.copy(
+      synoptic = "Synoptic1",
+      style = "Style1",
+      minutesUntilNextArrival = 11
+    )
+    val lineRemainingTime2 = lineRemainingTime.copy(
+      synoptic = "Synoptic2",
+      style = "Style2",
+      minutesUntilNextArrival = 12
+    )
+
+    val result = timesFactory.createLineRemainingTimeModels(
+      busStopRemainingTimes.copy(
+        lineRemainingTimes = listOf(lineRemainingTime1, lineRemainingTime2)
+      )
+    )
+
+    val listExpected = listOf(
+      LineRemainingTimeModel(
+        synopticModel = SynopticModel("Synoptic1", "Style1"),
+        minutesUntilNextArrival = 11
+      ),
+      LineRemainingTimeModel(
+        synopticModel = SynopticModel("Synoptic2", "Style2"),
+        minutesUntilNextArrival = 12
+      )
+    )
+
+    assertEquals(listExpected, result)
+  }
+
+  private val busStopRemainingTimes = BusStopRemainingTimes(
+    id = 0,
+    code = "",
+    name = "",
+    zone = "",
+    coordinates = Coordinates(0.0, 0.0),
+    lineRemainingTimes = emptyList()
+  )
+
+  private val lineRemainingTime = LineRemainingTime(
+    id = 0,
+    synoptic = "",
+    name = "",
+    style = "",
+    nextArrival = null,
+    minutesUntilNextArrival = 0
+  )
 }
