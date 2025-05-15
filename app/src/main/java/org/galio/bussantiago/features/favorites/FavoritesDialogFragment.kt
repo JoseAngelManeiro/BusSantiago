355de697d38696b560eae9572ecf5c5595d67561
José Ángel Maneiro <joseangel.maneiro@xing.com>
2025-05-15 10:50:57 +0200

    Expand always at maximum favorites dialog

--- app/src/main/java/org/galio/bussantiago/features/favorites/FavoritesDialogFragment.kt
+++ app/src/main/java/org/galio/bussantiago/features/favorites/FavoritesDialogFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.galio.bussantiago.databinding.FavoritesDialogFragmentBinding
 import org.galio.bussantiago.shared.BusStopFavoritesAdapter
@@ -28,10 +29,13 @@ class FavoritesDialogFragment : BottomSheetDialogFragment() {
       // Set the opacity level of the parent shadow behind dialog (1.0f is completely black)
       window?.setDimAmount(0.3f)
 
-      // Set transparency to dialog layer
       setOnShowListener {
+        // Set transparency to dialog layer
         val bottomSheet = findViewById<View>(materialR.id.design_bottom_sheet) as FrameLayout
         bottomSheet.setBackgroundResource(androidR.color.transparent)
+        // Expand the dialog to show all the content
+        val behavior = BottomSheetBehavior.from(bottomSheet)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
       }
     }
   }
