154ce7540d4a48f66117ab1bae7fd63d0ef4c8fb
José Ángel Maneiro <joseangel.maneiro@xing.com>
2025-07-19 12:23:32 +0200

    Upgrade to Api version 35

--- app/src/main/java/org/galio/bussantiago/features/MainActivity.kt
+++ app/src/main/java/org/galio/bussantiago/features/MainActivity.kt
@@ -2,11 +2,7 @@ package org.galio.bussantiago.features
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
 import org.galio.bussantiago.R
@@ -20,9 +16,6 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    // Use WindowCompat to opt into edge-to-edge layout
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-
     setContentView(R.layout.main_activity)
 
     // Let NavController automatically handle deep links when there is data
@@ -31,20 +24,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     reviewsHelper.initReviews()
-
-    // Optional: inset handling for padding
-    val rootView = findViewById<View>(R.id.navHostFragment)
-    ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      // Apply padding to avoid content being hidden behind system bars
-      view.setPadding(
-        systemBars.left,
-        systemBars.top,
-        systemBars.right,
-        systemBars.bottom
-      )
-      insets
-    }
   }
 
   override fun onNewIntent(intent: Intent?) {
