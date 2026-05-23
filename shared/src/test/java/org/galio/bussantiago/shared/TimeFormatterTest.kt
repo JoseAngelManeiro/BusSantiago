fa9c299ddf6a92dc7fade482eb325b93d1a540e6
JoseAngelManeiro <joseangelmaneiro@yahoo.es>
2026-05-23 10:53:36 +0200

    Adjust times info

--- shared/src/test/java/org/galio/bussantiago/shared/TimeFormatterTest.kt
+++ shared/src/test/java/org/galio/bussantiago/shared/TimeFormatterTest.kt
@@ -8,18 +8,8 @@ class TimeFormatterTest {
   private val timeFormatter = TimeFormatter()
 
   @Test
-  fun `if there are 0 mins until next arrival, the literal 'llegando' is returned`() {
-    assertEquals("llegando", timeFormatter.getDescription(0))
-  }
-
-  @Test
-  fun `if there are -1 mins until next arrival, the literal '1 min' is returned`() {
-    assertEquals("<1 min", timeFormatter.getDescription(-1))
-  }
-
-  @Test
-  fun `if there are -2 mins until next arrival, the literal 'en parada' is returned`() {
-    assertEquals("en parada", timeFormatter.getDescription(-2))
+  fun `if there are 0 mins until next arrival, the literal 'en parada' is returned`() {
+    assertEquals("en parada", timeFormatter.getDescription(0))
   }
 
   @Test
@@ -28,7 +18,12 @@ class TimeFormatterTest {
   }
 
   @Test
-  fun `if there are more than 59 minutes until next arrival, formatted time is returned`() {
+  fun `if there are exactly 60 minutes until next arrival, formatted time is returned`() {
+    assertEquals("1 h", timeFormatter.getDescription(60))
+  }
+
+  @Test
+  fun `if there are more than 60 minutes until next arrival, formatted time is returned`() {
     assertEquals("1 h 15 min", timeFormatter.getDescription(75))
   }
 }
--- shared/src/test/java/org/galio/bussantiago/shared/TimesFactoryTest.kt
+++ shared/src/test/java/org/galio/bussantiago/shared/TimesFactoryTest.kt
@@ -11,32 +11,37 @@ class TimesFactoryTest {
   private val timesFactory = TimesFactory()
 
   @Test
-  fun `should create the list of models as expected`() {
+  fun `should create the list of models as expected and not include negative values`() {
     val lineRemainingTime1 = lineRemainingTime.copy(
       synoptic = "Synoptic1",
       style = "Style1",
-      minutesUntilNextArrival = 11
+      minutesUntilNextArrival = -1
     )
     val lineRemainingTime2 = lineRemainingTime.copy(
       synoptic = "Synoptic2",
       style = "Style2",
-      minutesUntilNextArrival = 12
+      minutesUntilNextArrival = 2
+    )
+    val lineRemainingTime3 = lineRemainingTime.copy(
+      synoptic = "Synoptic3",
+      style = "Style3",
+      minutesUntilNextArrival = 9
     )
 
     val result = timesFactory.createLineRemainingTimeModels(
       busStopRemainingTimes.copy(
-        lineRemainingTimes = listOf(lineRemainingTime1, lineRemainingTime2)
+        lineRemainingTimes = listOf(lineRemainingTime1, lineRemainingTime2, lineRemainingTime3)
       )
     )
 
     val listExpected = listOf(
       LineRemainingTimeModel(
-        synopticModel = SynopticModel("Synoptic1", "Style1"),
-        minutesUntilNextArrival = 11
+        synopticModel = SynopticModel("Synoptic2", "Style2"),
+        minutesUntilNextArrival = 2
       ),
       LineRemainingTimeModel(
-        synopticModel = SynopticModel("Synoptic2", "Style2"),
-        minutesUntilNextArrival = 12
+        synopticModel = SynopticModel("Synoptic3", "Style3"),
+        minutesUntilNextArrival = 9
       )
     )
 
